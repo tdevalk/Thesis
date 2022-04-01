7eee5ea1c72ea6ef3384e2864dcf4c3a9da20890
tdevalk <61268107+tdevalk@users.noreply.github.com>
2022-04-01 11:10:58 +0200

    Experimented with missing data

--- Tryout.py
+++ Tryout.py
@@ -8,8 +8,7 @@ from pgmpy.estimators import BDeuScore, K2Score, BicScore, ExpectationMaximizati
 from pgmpy.models import BayesianModel, BayesianNetwork
 from pgmpy.estimators import HillClimbSearch, MaximumLikelihoodEstimator
 from FriedmanGoldszmidt import FG_estimator, SuffStatBicScore, MLE_FG
-
-
+from EM_imputation import ExpectationMaximizationImputation
 
 def rename_values(data: pd.DataFrame):
     data["gesl"].replace([1, 2], ["man", "vrouw"], inplace=True)
@@ -70,28 +69,47 @@ data = pd.read_csv("asia10K.csv")
 data = pd.DataFrame(data, columns=["Smoker", "LungCancer", "X-ray"])
 test_data = data[:2000]
 new_data = data[2000:]
+# new_data[:500]["Smoker"] = np.NaN
+
+
+
 
 #Create initial model from data
 bic = BicScore(test_data)
 hc = HillClimbSearch(test_data)
 model = hc.estimate(scoring_method=bic)
+bn = BayesianNetwork(model)
+bn.fit(test_data, estimator=MaximumLikelihoodEstimator)
 
 #Create Bic scorer that works with suff stats instead of data to evaluate structures
 bic_FG = SuffStatBicScore(test_data)
-bic_FG.calculate_sufficient_stats(model)
-bic_FG.score(model)
+bic_FG.calculate_sufficient_stats(bn)
+bic_FG.score(bn)
 
-FG = FG_estimator(test_data)
-updated_model = FG.update(new_data=new_data, scoring_method=bic_FG, start_dag=model, tabu_length=0,
-                          data_per_search=100)
+#Create Expectation Maximization scorer that works with suff stats instead of data to evaluate
+# parameters
+# EMI_FG = ExpectationMaximizationImputation(bn, test_data)
+# EMI_FG.set_suff_stats(bic_FG.suff)
+#
+#
+# FG = FG_estimator(test_data)
+# updated_model = FG.update(new_data=new_data, structure_scoring_method=bic_FG,
+#                           parameter_scoring_method=EMI_FG, start_dag=bn, tabu_length=0,
+#                           data_per_search=100)
 
-BN = BayesianNetwork(updated_model)
-mle_FG = MLE_FG(bic_FG.suff, BN, test_data)
-param = mle_FG.get_parameters()
-BN.add_cpds(*param)
-EM = ExpectationMaximization
+# BN = BayesianNetwork(model)
+# BN.fit(test_data, estimator=MaximumLikelihoodEstimator)
+# mle_FG = MLE_FG(bic_FG.suff, BN, test_data)
+# param = mle_FG.get_parameters()
+# BN.add_cpds(*param)
+# EM = ExpectationMaximization
 
-# print(model.edges())
-# fig = plt.figure()
-# graph = nx.draw(best_model, with_labels=True, pos=nx.spring_layout(best_model))
-# plt.show()
+# new_data["Smoker"][:500] = np.NaN
+# new_data["X-ray"][400:600] = np.NaN
+#
+# bn = BayesianNetwork(model)
+# bn.fit(new_data, estimator=ExpectationMaximizationImputation, complete_samples_only=False)
+# print("end")
+
+
+
